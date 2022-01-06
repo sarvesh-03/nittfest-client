59d57bf7b04bf821a6a951dcb29d29eacfc3ed71
muhesh <vmspride7@gmail.com>
2022-01-06 21:46:41 +0530

    feat(formsPage): make forms page responsive

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class NITTFEST extends StatelessWidget {
       onUnknownRoute: (settings) {
         Get.offAndToNamed(NavigationRoutes.splashRoute);
       },
-      initialRoute: NavigationRoutes.inductionsHomeRoute,
+      initialRoute: NavigationRoutes.inductionsFormsRoute,
       getPages: NavigationPages.getPages(),
     );
   }
--- lib/views/pages/forms/responsive/forms_page_desktop.dart
+++ lib/views/pages/forms/responsive/forms_page_desktop.dart
@@ -11,7 +11,7 @@ class FormsPagePhone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var controller = Get.find<FormsController>();
-
+    
     return Stack(children: [
       Positioned(
           top: 10,
